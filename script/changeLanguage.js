fed89263dc742ce03c40f57dea2117544efd4538
Diennen <141672207+Diennen@users.noreply.github.com>
2024-02-13 02:50:58 +0300

    Add files via upload

---
+++ script/changeLanguage.js
@@ -0,0 +1,423 @@
+const langArr = {
+	// Header
+	"header-link-1": {
+		"en": "About us",
+		"pl": "O nas",
+		"fr": "À propos de nous",
+	},
+
+	"header-link-2": {
+		"en": "Services",
+		"pl": "Usługi",
+		"fr": "Services",
+	},
+
+	"header-link-3": {
+		"en": "Home",
+		"pl": "Strona główna",
+		"fr": "Accueil",
+	},
+
+	"header-link-4": {
+		"en": "Portfolio",
+		"pl": "Portfolio",
+		"fr": "Portefeuille",
+	},
+
+	// Burger
+	"burger-link-1": {
+		"en": "About us",
+		"pl": "O nas",
+		"fr": "À propos de nous",
+	},
+
+	"burger-link-2": {
+		"en": "Services",
+		"pl": "Usługi",
+		"fr": "Services",
+	},
+
+	"burger-link-3": {
+		"en": "Home",
+		"pl": "Strona główna",
+		"fr": "Accueil",
+	},
+
+	"burger-link-4": {
+		"en": "Portfolio",
+		"pl": "Portfolio",
+		"fr": "Portefeuille",
+	},
+
+	"contact-social-title": {
+		"en": "Follow us on:",
+		"pl": "Jesteśmy w mediach:",
+		"fr": "Suivez-nous:",
+	},
+
+	// Main
+	"intro-title": {
+		"en": "Designing and working with stands",
+		"pl": "Projektowanie i praca ze stoiskami",
+		"fr": "Conception et réalisation de stands",
+	},
+
+	"intro-subtitle": {
+		"en": "Creating innovative and functional solutions to capture attention for your brand",
+		"pl": "Tworzenie innowacyjnych i funkcjonalnych rozwiązań, aby przyciągnąć uwagę do Twojej marki",
+		"fr": "Création de solutions innovantes et fonctionnelles pour attirer l'attention sur votre marque",
+	},
+
+	"intro-buttons-services": {
+		"en": "Services",
+		"pl": "Usługi",
+		"fr": "Services",
+	},
+
+	"intro-buttons-portfolio": {
+		"en": "Portfolio",
+		"pl": "Portfolio",
+		"fr": "Portefeuille",
+	},
+	// About-us
+	"about-us-title": {
+		"en": "About us",
+		"pl": "O nas",
+		"fr": "À propos de nous",
+	},
+
+	"about-us-subtitle": {
+		"en": "<p>Our dynamically growing company, headquartered in Wroclaw (Poland), specializes in high-quality services related to the assembly and disassembly of stands, as well as the production and efficient transportation of materialsk.</p><p>We delivering our professional services across the territories of the European Union, the United Kingdom, and the United States, aiming to provide customers with an outstanding level of service and satisfaction.</p>",
+		"pl": "<p>Nasza dynamicznie rozwijająca się firma, z siedzibą we Wrocławiu (Polska), specjalizuje się w usługach wysokiej jakości związanych z montażem i demontażem stoisk, a także produkcją i efektywnym transportem materiałów.</p><p>Świadczymy nasze profesjonalne usługi na terenie Unii Europejskiej, Wielkiej Brytanii i Stanów Zjednoczonych, dążąc do zapewnienia klientom wyjątkowego poziomu obsługi i satysfakcji.</p>",
+		"fr": "<p>Notre entreprise en croissance dynamique, dont le siège est à Wroclaw (Pologne), est spécialisée dans les services de haute qualité liés au montage et au démontage de stands, ainsi qu'à la production et au transport efficace des matériaux.</p><p>Nous fournissons nos services professionnels sur les territoires de l'Union européenne, du Royaume-Uni et des États-Unis, dans le but de fournir à nos clients un niveau de service et de satisfaction exceptionnel.</p>",
+	},
+
+	// Services
+	"services-title": {
+		"en": "Our services",
+		"pl": "Nasze usługi",
+		"fr": "Nos services",
+	},
+
+	// Cards
+	// Card 1
+	"services-card-title-1": {
+		"en": "Stand construction",
+		"pl": "Budowa stoisk",
+		"fr": "Construction de stands",
+	},
+
+	"services-card-subtitle-1": {
+		"en": "Individual solutions for crafting unique exhibition spaces. We specialize in providing comprehensive solutions that ensure a seamless and memorable exhibition experience.",
+		"pl": "Indywidualne rozwiązania w celu stworzenia unikalnych przestrzeni wystawienniczych. Specjalizujemy się w dostarczaniu kompleksowych rozwiązań, które zapewniają ciągłe i pamiętne doświadczenie z wystawy.",
+		"fr": "Des solutions individuelles pour créer des espaces d’exposition uniques. Nous sommes spécialisés dans la fourniture de solutions complètes qui garantissent une expérience d’exposition fluide et mémorable.",
+	},
+
+	// Card 2
+	"services-card-title-2": {
+		"en": "Stand dismantling",
+		"pl": "Demontaż stoisk",
+		"fr": "Démontage des stands",
+	},
+
+	"services-card-subtitle-2": {
+		"en": "Streamlining your event closure process. Our dedicated team ensures the swift and meticulous disassembly of stands, allowing you to wrap up your event seamlessly, without any hassle. Experience hassle-free teardowns with our expert dismantling services.",
+		"pl": "Optymalizacja procesu zakończenia Państwa wydarzenia. Nasz dedykowany zespół zapewnia szybki i dokładny demontaż stoisk, umożliwiając łatwe i bezproblemowe zakończenie Państwa wydarzenia. Poczuj wygodę z naszymi specjalistycznymi usługami demontażu.",
+		"fr": "Rationalisation du processus de clôture de votre événement. Notre équipe dédiée assure le démontage rapide et minutieux des stands, vous permettant ainsi de clôturer votre événement en toute fluidité, sans tracas. Vivez des démontages sans tracas grâce à nos services de démontage experts.",
+	},
+
+	// Card 3
+	"services-card-title-3": {
+		"en": "Material Crafting",
+		"pl": "Produkcja materiałów",
+		"fr": "Fabrication de matériaux",
+	},
+
+	"services-card-subtitle-3": {
+		"en": "Provision of individual solutions, starting from creative concepts and ending with experienced implementation. Trust us in creating outstanding materials for stands that will not only distinguish your brand but also capture the attention of your audience.",
+		"pl": "Oferowanie indywidualnych rozwiązań, począwszy od kreatywnych koncepcji, a skończywszy na doświadczonej realizacji. Powierz nam stworzenie wyjątkowych materiałów do stoisk, które nie tylko wyróżnią Twój brand, ale także przyciągną uwagę Twojej publiczności.",
+		"fr": "Fournir des solutions individuelles, depuis les concepts créatifs jusqu'à la réalisation expérimentée. Faites-nous confiance pour créer des matériaux exceptionnels pour les stands qui non seulement mettront en valeur votre marque, mais attireront l'attention de votre public.",
+	},
+
+	// Card 4
+	"services-card-title-4": {
+		"en": "Material Transport",
+		"pl": "Transport materiałów",
+		"fr": "Transport des matériaux",
+	},
+
+	"services-card-subtitle-4": {
+		"en": "We ensure efficient and reliable material transportation, delivering professional solutions for your business.",
+		"pl": "Zapewniamy efektywny i niezawodny transport materiałów, oferując profesjonalne rozwiązania dla Twojego biznesu.",
+		"fr": "Nous assurons un transport efficace et fiable des matériaux, en fournissant des solutions professionnelles pour votre entreprise.",
+	},
+
+	// Card 5
+	"services-card-title-5": {
+		"en": "3D stand mockups",
+		"pl": "Modele 3D stoisk",
+		"fr": "Maquettes 3D des stands",
+	},
+
+	"services-card-subtitle-5": {
+		"en": "Bringing your future event to life. We craft realistic 3D models, offering you the opportunity to visualize every detail of your upcoming stand. Trust us to create a virtual space that will showcase your brand.",
+		"pl": "Odtworzenie Twojego przyszłego wydarzenia. Tworzymy realistyczne modele 3D, dając Ci możliwość wizualizacji każdego detalu Twojego przyszłego stoiska. Zaufaj nam w tworzeniu wirtualnej przestrzeni, która wyróżni Twój brand.",
+		"fr": "Donner vie à votre futur prochain événement. Nous créons des maquettes 3D réalistes, vous offrant la possibilité de visualiser chaque détail de votre futur stand. Faites-nous confiance pour créer un espace virtuel qui mettra en valeur votre marque.",
+	},
+
+	// Card 6
+	"services-card-title-6": {
+		"en": "We operate",
+		"pl": "Pracujemy",
+		"fr": "Nous travaillons",
+	},
+
+	"services-card-subtitle-6": {
+		"en": "Our company provides services both on the national market and on the global market. We operate successfully in the territories of the European Union, the United States, and the United Kingdom, ensuring high-quality service across an extensive area.",
+		"pl": "Nasza firma świadczy usługi zarówno na rynku krajowym, jak i na rynku globalnym. Skutecznie działamy na terenie Unii Europejskiej, Stanów Zjednoczonych i Wielkiej Brytanii, zapewniając wysoką jakość obsługi na obszernym terenie.",
+		"fr": "Notre société fournit des services tant sur le marché national que sur le marché mondial. Nous opérons avec succès dans l'Union européenne, aux États-Unis et au Royaume-Uni, offrant un service de qualité sur une vaste étendue.",
+	},
+
+	// Portfolio
+	"portfolio-title": {
+		"en": "Our portfolio",
+		"pl": "Nasze portfolio",
+		"fr": "Notre portefeuille",
+	},
+
+	"portfolio-subtitle": {
+		"en": "Explore a collection of our best works that speak to our commitment to quality, innovation, and customer satisfaction. From outstanding designs to successful projects, our portfolio is a testament to our dedication to providing exceptional solutions.",
+		"pl": "Odkryj kolekcję naszych najlepszych prac, które świadczą o naszym dążeniu do jakości, innowacji i zadowolenia klientów. Od wybitnych projektów po udane realizacje, nasze portfolio to dowód na nasze pragnienie oferowania wyjątkowych rozwiązań.",
+		"fr": "Découvrez la collection de nos meilleurs travaux, témoignant de notre engagement envers la qualité, l'innovation et la satisfaction des clients. Des designs exceptionnels aux projets réussis, notre portfolio est la preuve de notre volonté de fournir des solutions exceptionnelles.",
+	},
+
+	"potfolio-links-element-background-text-1": {
+		"en": "Stands",
+		"pl": "Stoiska",
+		"fr": "Stands",
+	},
+
+	"potfolio-links-element-background-text-2": {
+		"en": "3D mockups",
+		"pl": "Modele 3D",
+		"fr": "Maquettes 3D",
+	},
+
+	// Footer
+	"footer-info-title": {
+		"en": "Contacts",
+		"pl": "Kontakty",
+		"fr": "Contacts",
+	},
+
+	"footer-social-title": {
+		"en": "Social",
+		"pl": "Sociale",
+		"fr": "Suivez-nous:",
+	},
+
+	"footer-nav-title": {
+		"en": "Navigation",
+		"pl": "Nawigacja",
+		"fr": "Navigation",
+	},
+
+	"footer-subtitle-1": {
+		"en": "Home",
+		"pl": "Strona główna",
+		"fr": "Accueil",
+	},
+
+	"footer-subtitle-2": {
+		"en": "Portfolio",
+		"pl": "Portfolio",
+		"fr": "Portefeuille",
+	},
+
+	"footer-copyright-element-1": {
+		"en": "2023-2024 © LLC “A2EXPO”",
+		"pl": "2023-2024 © Sp. z o.o. “A2EXPO”",
+		"fr": "2023-2024 © SARL “A2EXPO”",
+	},
+
+	"footer-copyright-element-2": {
+		"en": "General Wroblewski Square 3A/7, 50-413 Wroclaw (legal address)",
+		"pl": "pl. gen. Wróblewskiego 3A/7 50-413 Wrocław (adres prawny)",
+		"fr": "Place du Général Wroblewski 3A/7, 50-413 Wroclaw (adresse juridique)",
+	},
+
+	"footer-copyright-element-3": {
+		"en": "TIN: 8992950765",
+		"pl": "NIP: 8992950765",
+		"fr": "NIF: 8992950765",
+	},
+
+	// Portfolio
+	"portfolio-title": {
+		"en": "Portfolio",
+		"pl": "Portfolio",
+		"fr": "Portefeuille",
+	},
+
+	"potfolio-subtitle": {
+		"en": "Visualizing our creativity and expertise. Each project is a unique piece of art, embodying our commitment to perfection. Immerse yourself in the world of our solutions and appreciate the quality that makes us your ideal partner in achieving your goals.",
+		"pl": "Wizualizacja naszej kreatywności i profesjonalizmu. Każdy projekt to unikatowe dzieło sztuki, które symbolizuje nasze dążenie do doskonałości. Zanurz się w świecie naszych rozwiązań i doceniaj jakość, która czyni nas idealnym partnerem w osiąganiu Twoich celów.",
+		"fr": "Visualisation de notre créativité et de notre professionnalisme. Chaque projet est une œuvre d'art unique, représentant notre engagement envers l'excellence. Plongez dans le monde de nos solutions et appréciez la qualité qui fait de nous votre partenaire idéal pour atteindre vos objectifs.",
+	},
+
+	"portfolio-choise-element-1": {
+		"en": "Stands",
+		"pl": "Stoiska",
+		"fr": "Stands",
+	},
+
+	"portfolio-choise-element-2": {
+		"en": "Mockups",
+		"pl": "Mockupy",
+		"fr": "Maquettes",
+	},
+}
+
+
+
+
+
+
+
+const headerLanguages = document.querySelector('.header-languages-menu');
+const activeLanguage = document.querySelector('.header-active-language');
+const languageButtons = headerLanguages.querySelectorAll('.header-languages-element-option');
+const allLang = ['en', 'pl', 'fr'];
+const defLang = 'en';
+let selectedLanguage = localStorage.getItem('selectedLng');
+let choosedLanguageAfter = checkChoosedLanguage();
+
+localStorage.setItem('selectedLng', choosedLanguageAfter);
+
+allLanguageFunctions();
+
+// --------------------------------------------------
+for (let i = 0; i < languageButtons.length; i++) {
+	languageButtons[i].addEventListener('click', allLanguageFunctions);
+}
+
+function allLanguageFunctions(event) {
+	changeURLLanguage(event);
+	changeActiveLanguage(event);
+	// changeHeadLanguageOptions(event);
+	changeLanguage(event);
+	reloadPageIFClick(event);
+}
+
+// Проверка языка из хранилища
+function checkChoosedLanguage() {
+	let choosedLanguage;
+
+	for (let i = 0; i < allLang.length; i++) {
+		if (selectedLanguage === allLang[i]) {
+			choosedLanguage = allLang[i];
+			break;
+		}
+		else {
+			choosedLanguage = defLang;
+		}
+	}
+	return choosedLanguage;
+}
+
+// Показ выбранного языка в меню
+function changeActiveLanguage(event) {
+	if (event) {
+		const choosedLanguage = event.currentTarget;
+		const choosedLanguageHTML = choosedLanguage.outerHTML;
+		activeLanguage.innerHTML = choosedLanguageHTML;
+	}
+	else {
+		const attrToSearch = choosedLanguageAfter;
+		const attr = `[data-lang-value="${attrToSearch}"]`
+		const lang = document.querySelector(attr);
+		const langHTML = lang.outerHTML;
+		activeLanguage.innerHTML = langHTML;
+	}
+}
+
+// Добавление языка в URL
+function changeURLLanguage(event) {
+	if (event) {
+		const pressedButton = event.currentTarget;
+		const attr = pressedButton.getAttribute('data-lang-value')
+		let lang = attr;
+		location.href = window.location.pathname + "#" + lang;
+	}
+	else {
+		location.href = window.location.pathname + "#" + choosedLanguageAfter;
+	}
+}
+
+// Смена языка
+function changeLanguage(event) {
+	const htmlElement = document.querySelector('html');
+	const headDescription = document.querySelector('meta[name="description"]');
+	const headKeyWords = document.querySelector('meta[name="keywords"]');
+
+	let hash = window.location.hash;
+	hash = hash.substr(1);
+
+	// Проверка на доступные языки
+	if (!allLang.includes(hash)) {
+		// #en - язык по умолчанию
+		location.href = window.location.pathname + '#' + choosedLanguageAfter;
+		location.reload();
+	}
+
+
+	// Запись выбранного языка в LocalStorage
+	localStorage.setItem('selectedLng', hash);
+
+	// Смена у head
+	if (hash === "en") {
+		htmlElement.setAttribute('lang', 'en');
+		headDescription.setAttribute('content', 'Creating layouts, stands, assembling advertisements');
+		headKeyWords.setAttribute('content', 'a2expo, expo, creation, layouts, stands, advertising assembly, web design, online advertising, advertising graphics, ad creation, advertising services, trade show stands, ad assembly, advertising campaigns');
+		document.title = "A2EXPO - Creating layouts, stands, assembling advertisements";
+	}
+
+	else if (hash === "pl") {
+		htmlElement.setAttribute('lang', 'pl');
+		headDescription.setAttribute('content', 'Tworzenie układów, stoisk, montaż reklam');
+		headKeyWords.setAttribute('content', 'a2expo, expo, tworzenie, layouty, standy, montaż reklam, projektowanie stron, reklama online, grafika reklamowa, kreacja reklam, usługi reklamowe, standy targowe, składanie reklam, kampanie reklamowe');
+		document.title = "A2EXPO - Tworzenie układów, stoisk, montaż reklam";
+	}
+
+	else if (hash === "fr") {
+		htmlElement.setAttribute('lang', 'fr');
+		headDescription.setAttribute('content', 'Création de mises en page, de stands, montage de publicités');
+		headKeyWords.setAttribute('content', 'a2expo, expo, création, mises en page, stands, montage publicitaire, conception de sites web, publicité en ligne, graphisme publicitaire, création publicitaire, services publicitaires, montage publicitaire, campagnes publicitaires');
+		document.title = "A2EXPO - Création de mises en page, de stands, montage de publicités";
+	}
+
+	else {
+		htmlElement.setAttribute('lang', 'en');
+		headDescription.setAttribute('content', 'Creating layouts, stands, assembling advertisements');
+		headKeyWords.setAttribute('content', 'creation, layouts, stands, advertising assembly, web design, online advertising, advertising graphics, ad creation, advertising services, trade show stands, ad assembly, advertising campaigns');
+		document.title = "A2EXPO - Creating layouts, stands, assembling advertisements";
+	}
+
+	for (let key in langArr) {
+		let elem = document.querySelector('.lng-' + key);
+		// Проверка на наличие переводимого элемента, потом смена языка
+		if (elem) {
+			elem.innerHTML = langArr[key][hash];
+		}
+	}
+}
+
+// Перезагрузка страницы, если пользователь выбрал язык
+function reloadPageIFClick(event) {
+	if (event) {
+		// location.reload();
+	}
+}
+
+
+
